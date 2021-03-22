2262a123ef41ff9740a5ee347a3c5e22899c5117
Grace Bong <gracebong.park@gmail.com>
2021-03-22 10:08:39 -0400

    personal portfolio

--- index.js
+++ index.js
@@ -1,37 +1,68 @@
-let date = {
-    months: {
-        '01': 'January',
-        '02': 'February',
-        '03': 'March',
-        '04': 'April',
-        '05': 'May',
-        '06': 'June',
-        '07': 'July',
-        '08': 'August',
-        '09': 'September',
-        '10': 'October',
-        '11': 'November',
-        '12': 'December'
-    },
-    year: {},
-    day: {}
-}
-
 fetch('http://www.7timer.info/bin/api.pl?lon=113.17&lat=23.09&product=civillight&output=json')
     .then(response => response.json())
     .then(body => {
-        console.log(body)
+        let dateInfo = {
+            months: {
+                1: 'January',
+                2: 'February',
+                3: 'March',
+                4: 'April',
+                5: 'May',
+                6: 'June',
+                7: 'July',
+                8: 'August',
+                9: 'September',
+                10: 'October',
+                11: 'November',
+                12: 'December'
+            },
+            year: {},
+            day: {},
+            month: {}
+        }
+
         let weather = document.getElementById('weather')
-        for (let i = 0; i < body.dataseries.length; i += 1) {
+        for (let i = 0; i < 1; i += 1) {
             weather.innerHTML = `${body.dataseries[0].weather} | ${body.dataseries[0].temp2m.min}&#x2109 - ${body.dataseries[0].temp2m.max}&#x2109`    
         }
-
+        
         let date = document.getElementById('date')
         let apiDate = body.dataseries[0].date
-        for (let i = 0; i < apiDate.length; i += 1) {
-            date.textContent = apiDate
-
-        }
+        let stringDate = JSON.stringify(apiDate)
         
+        for (let i = 0; i < stringDate.length; i += 1) {
+            if (i === 0) {
+                dateInfo.year = stringDate[i]
+            } else if (i === 1) {
+                dateInfo.year += stringDate[i]
+            } else if (i === 2) {
+                dateInfo.year += stringDate[i]
+            } else if (i === 3) {
+                dateInfo.year += stringDate[i]
+            } else if (i === 4) {
+                dateInfo.month = stringDate[i]
+            } else if (i === 5) {
+                dateInfo.month += stringDate[i]
+            } else if (i === 6) {
+                dateInfo.day = stringDate[i]
+            } else if (i === 7) {
+                dateInfo.day += stringDate[i]
+            }
+        }
+
+        let month
+        let numMonth = Number(dateInfo.month)
+        for (let key in dateInfo.months) {
+            if (dateInfo.months[numMonth]) {
+                month = dateInfo.months[numMonth]
+            }
+        }
+        date.textContent = `${month} ${dateInfo.day}, ${dateInfo.year}`
     })
     .catch(error => console.log(error))
+
+// switchStyle => {}
+
+joke () => {
+    fetch('')
+}
